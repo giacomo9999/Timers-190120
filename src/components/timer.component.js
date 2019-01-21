112edcc8d1cc08eafd12d02899c77acf3783e935
James Gary <gmarchesi888@gmail.com>
2019-01-21 12:46:29 -0500

    Styling more or less complete.

--- src/components/timer.component.js
+++ src/components/timer.component.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "semantic-ui-react";
+import { Card, Icon, Button, Image } from "semantic-ui-react";
 
 class Timer extends React.Component {
   render() {
@@ -7,7 +7,19 @@ class Timer extends React.Component {
       <Card fluid>
         <Card.Content>
           <Card.Header>Timer </Card.Header>
+          <Card.Meta>{this.props.title} </Card.Meta>
+          <Card.Meta>{this.props.project} </Card.Meta>
+          <Card.Description as="h2">{this.props.elapsed}</Card.Description>
         </Card.Content>
+        <div className="extra content">
+          <span className="right floated edit icon">
+            <i className="edit icon" />
+          </span>
+          <span className="right floated trash icon">
+            <i className="trash icon" />
+          </span>
+        </div>
+        <Button color="blue">Start</Button>
       </Card>
     );
   }
